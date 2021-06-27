d471b9f61e589da831e9615b8dc8da991c96b16c
ahmed2nabil <ahmednabl992@gmail.com>
2021-06-27 10:26:23 +0200

    'modify_year_model'

--- models/year.js
+++ models/year.js
@@ -7,18 +7,27 @@ const students= new Schema({
     type:mongoose.Schema.Types.ObjectId, 
     ref:'students'
     }
-})
+},{"_id" : false});
+const classes =new Schema({
+    classID:
+        {type:mongoose.Schema.Types.ObjectId,
+        ref:'classes'}
+},{"_id" : false});
 const yearSchema = new Schema({
     name:
     {type: String,
     required:true },
+    yearNumber : {
+        type: Number
+    },
     deptId:
     {
         type: mongoose.Schema.Types.ObjectId,
         ref:'department'
     },
-    students:[students]
-
+    students:[students],
+    classes : [classes],
+    classesOLD : [classes],
 })
 yearSchema.plugin(passportLocalMongoose);//make schema support passport-local-mongoose
 var years=mongoose.model('year',yearSchema);
