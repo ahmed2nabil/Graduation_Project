c28729dfd18e756267028aa0ec98e07721bf0322
BasmaEsmail <52723710+BasmaEsmail@users.noreply.github.com>
2021-06-30 18:18:12 +0200

    Update department.js

--- models/department.js
+++ models/department.js
@@ -7,6 +7,18 @@ const staff=new Schema({
         ref:'teachingstaff'}
 }, {"_id" : false});
 
+const Assistants=new Schema({
+    assistantId:
+        {type:mongoose.Schema.Types.ObjectId,
+        ref:'teachingAssistants'}
+}, {"_id" : false});
+
+const Graduates=new Schema({
+    graduateId:
+        {type:mongoose.Schema.Types.ObjectId,
+        ref:'graduates'}
+}, {"_id" : false}); 
+
 const deptSchema=new Schema({
     id:
     {
@@ -14,7 +26,9 @@ const deptSchema=new Schema({
         required:true
     },
     name:{type:String, required:true},
-    staff:[staff]
+    staff:[staff],
+    teachingassistants:[Assistants],
+    graduates:[Graduates]
 });
 var dept= mongoose.model('department',deptSchema);
 module.exports=dept;
